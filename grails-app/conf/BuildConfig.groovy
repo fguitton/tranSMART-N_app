71ce89240a057acdc4b28d729833669ccd666f95
Haiping Xia <haxia@deloitte.com>
2013-10-18 22:35:49 -0400

    Use Netezza JDBC driver

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,7 +45,7 @@ grails.project.dependency.resolution = {
         ])
     }
     dependencies {
-		runtime 'postgresql:postgresql:9.0-801.jdbc4'
+		//runtime 'postgresql:postgresql:9.0-801.jdbc4'
 		compile 'antlr:antlr:2.7.7'
         compile 'org.transmartproject:transmart-core-api:1.0-SNAPSHOT'
     }
